b630ac85235c443d09ab9c37941bad058b0ac16f
andres jaramillo <andres.jaramillo@celuweb.com>
2023-03-28 15:09:32 -0500

    Finish SwiftUIScrollView lecture

--- SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HeaderView(date: "Monday, aug 20", title: "Your Reading")
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    Group{
+                        CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                        CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                        CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                        CardView(image: "natural-language-api", category: "iOS", heading: "What's Newin Natural Language API", author: "Sai Kambampati")
+                    }
+                    .frame(width: 300)
+                }
+            }
+            Spacer()
         }
         .padding()
     }
