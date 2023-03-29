5c7ded607e780ff81c428d0331a44f32c58b9c89
amjr90 <amjr.90@gmail.com>
2023-03-29 09:58:07 -0500

    Create initial project for SwiftUIState

---
+++ SwiftUIState/SwiftUIState/SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateApp.swift
+//  SwiftUIState
+//
+//  Created by andres jaramillo on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
