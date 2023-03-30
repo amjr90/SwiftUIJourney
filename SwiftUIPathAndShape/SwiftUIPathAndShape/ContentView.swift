89b64063c654fda20f8e10f43b220def093dac47
amjr90 <amjr.90@gmail.com>
2023-03-29 21:37:47 -0500

    Create a Rectangle and a rectangle with a dome using path

--- SwiftUIPathAndShape/SwiftUIPathAndShape/ContentView.swift
+++ SwiftUIPathAndShape/SwiftUIPathAndShape/ContentView.swift
@@ -10,17 +10,54 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            RectangleShape()
+            DomeRectangleShape()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct RectangleShape: View {
+    var body: some View {
+        Path(){ path in
+            path.move(to: CGPoint(x: 20, y: 20))
+            path.addLine(to: CGPoint(x: 300, y: 20))
+            path.addLine(to: CGPoint(x: 300, y: 200))
+            path.addLine(to: CGPoint(x: 20, y: 200))
+            path.closeSubpath()
+        }
+        .stroke(.green, lineWidth: 10)
+    }
+}
+
+struct DomeRectangleShape: View {
+    var body: some View {
+        ZStack {
+            Path(){path in
+                path.move(to: CGPoint(x: 20, y: 60))
+                path.addLine(to: CGPoint(x: 40, y: 60))
+                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
+                path.addLine(to: CGPoint(x: 230, y: 60))
+                path.addLine(to: CGPoint(x: 230, y: 100))
+                path.addLine(to: CGPoint(x: 20, y: 100))
+            }
+            .fill(.purple)
+            
+            Path(){path in
+                path.move(to: CGPoint(x: 20, y: 60))
+                path.addLine(to: CGPoint(x: 40, y: 60))
+                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
+                path.addLine(to: CGPoint(x: 230, y: 60))
+                path.addLine(to: CGPoint(x: 230, y: 100))
+                path.addLine(to: CGPoint(x: 20, y: 100))
+                path.closeSubpath()
+            }
+            .stroke(.black, lineWidth: 3)
+        }
     }
 }
