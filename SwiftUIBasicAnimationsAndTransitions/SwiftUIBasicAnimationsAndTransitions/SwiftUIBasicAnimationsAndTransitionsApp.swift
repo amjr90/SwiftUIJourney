da36fbb9317304af00b3feb1380f292ca2095b3a
amjr90 <amjr.90@gmail.com>
2023-03-30 18:33:21 -0500

    Create initial project for basic animations and transtions

---
+++ SwiftUIBasicAnimationsAndTransitions/SwiftUIBasicAnimationsAndTransitions/SwiftUIBasicAnimationsAndTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicAnimationsAndTransitionsApp.swift
+//  SwiftUIBasicAnimationsAndTransitions
+//
+//  Created by andres jaramillo on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicAnimationsAndTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
