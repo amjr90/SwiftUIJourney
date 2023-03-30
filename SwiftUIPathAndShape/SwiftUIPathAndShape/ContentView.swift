2ad76ad782b1faf73cad63bb434621cad1b0ac7e
amjr90 <amjr.90@gmail.com>
2023-03-30 14:28:59 -0500

    Create a button using a custom shape

--- SwiftUIPathAndShape/SwiftUIPathAndShape/ContentView.swift
+++ SwiftUIPathAndShape/SwiftUIPathAndShape/ContentView.swift
@@ -10,9 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        //            RectangleShape()
-        //            DomeRectangleShape()
-        PieChart()
+        //        RectangleShape()
+        //        DomeRectangleShape()
+        //        PieChart()
+        Button(action: {
+            // Action to perform
+        }) {
+            Text("Test")
+                .font(.system(.title, design: .rounded))
+                .bold()
+                .foregroundColor(.white)
+                .frame(width: 250, height: 50)
+                .background(Dome().fill(Color.red))
+        }
         
     }
 }
@@ -98,5 +108,16 @@ struct PieChart: View {
                 
             }
         }
+    }
+}
+
+struct Dome: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addQuadCurve(to: CGPoint(x: rect.size.width, y: 0), control: CGPoint(
+            x: rect.size.width/2, y: -(rect.size.width * 0.1)))
+        path.addRect(CGRect(x: 0, y: 0, width: rect.size.width, height: rect.size.height))
+        return path
     }
 }
